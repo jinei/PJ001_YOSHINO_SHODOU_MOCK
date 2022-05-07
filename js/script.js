a9c80ac64c3313859b3df84c0abc99d1d279e834
Jinei <jinei.pf@gmail.com>
2022-05-07 19:25:36 +0900

    スライダー画像

--- js/script.js
+++ js/script.js
@@ -9,6 +9,33 @@ window.onload = function () {
     arrows: false
   });
 
+  $('.img_slider').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 6900,
+    infinite: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    cssEase: 'linear',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1.5,
+        }
+      }
+    ]
+  });
+
 };
 
 $(".header_sp_menu_btn").click(function () {
