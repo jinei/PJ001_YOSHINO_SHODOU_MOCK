cd9a194cee756bb34f16e3a94b9d88f8012c8f29
Jinei <jinei.pf@gmail.com>
2022-05-17 21:51:49 +0900

    トップ ファーストビュー修正

--- js/top.js
+++ js/top.js
@@ -1,8 +1,29 @@
-// トップのスライダー
-$('.slider').slick({
-  speed: 1500,
-  fade: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: false
+$(function () {
+  $(".slider")
+    .on("init", function () {
+      $('.slick-slide[data-slick-index="0"]').addClass("slider_zoom");
+    })
+    .slick({
+      autoplay: true,
+      fade: true,
+      arrows: false,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+    })
+    .on({
+      beforeChange: function (event, slick, currentSlide, nextSlide) {
+        $(".slick-slide", this).eq(nextSlide).addClass("slider_zoom");
+        $(".slick-slide", this).eq(currentSlide).addClass("remove-animation");
+      },
+      afterChange: function () {
+        $(".remove-animation", this).removeClass(
+          "remove-animation slider_zoom"
+        );
+      },
+    });
 });
+
+
+
